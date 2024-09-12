062dc8787f5bab0bf5af62b26eed82ea3744571b
= <=>
2024-09-12 17:10:53 +0200

    queires

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Traceback
-General code for performing trajectory traceback from AIU traceback code
+General code for performing trajectory traceback from AIU traceback code.
+
+## Project 1: SNR G166
+Green's catalog: https://www.mrao.cam.ac.uk/surveys/snrs/snrs.data.html
+
+on vizier table "VII/272/snrs": https://vizier.cds.unistra.fr/viz-bin/VizieR?-source=VII/272
+
+### Summary G166
+
+https://www.mrao.cam.ac.uk/surveys/snrs/snrs.G166.0+4.3.html
+
+Right Ascension: 05 26 30 
+
+Declination: +42 56
+
+Size (/arcmin): 55×35
+
+Type: S
+
+Flux density at 1 GHz (/Jy): 7
+
+Spectral Index: 0.37
+
+Radio: Two arcs of strikingly different radii.
+
+Optical: Nearly complete ring.
+
+X-ray: Predominantly in SW.
+
+Distance: HI indicates 4.5 kpc, optical extinction suggests 3.2 kpc.
---
+++ queries.py
@@ -0,0 +1,106 @@
+from astropy.coordinates import SkyCoord
+import numpy as np
+from astropy.table import Table, join, vstack
+from astropy.time import Time
+from astropy import units as u
+import time
+from astropy.coordinates.angles import Angle
+from astroquery.vizier import Vizier
+from astropy.utils.metadata import MergeConflictWarning
+import warnings
+
+def get_catalog(name):
+    '''
+    ### example:
+    green = get_catalog("VII/272/snrs")
+    '''
+    return Vizier(columns=["*", "+_r"], row_limit=-1).get_catalogs(name)[0]
+
+def searchDR3(skycoordinate: SkyCoord, radius: Angle) -> Table:
+    start_time = time.time()
+    print(f"Searching in Gaia DR3 I/355/gaiadr3 {radius} around {skycoordinate.ra, skycoordinate.dec, skycoordinate.distance}")
+    filters = {'Gmag': '<17', 'Plx': '>0.3'}
+    stars_fromDR3 = Vizier(columns=["*", "+_r"], row_limit=-1).query_region(
+        skycoordinate, 
+        radius=radius, 
+        catalog="I/355/gaiadr3",
+        column_filters=filters
+    )[0]  
+    stars_fromDR3['SkyCoord1'] = SkyCoord(
+        ra=stars_fromDR3['RA_ICRS'],
+        dec=stars_fromDR3['DE_ICRS'],
+        distance=(stars_fromDR3['Plx']).to(u.pc, equivalencies=u.parallax()),
+        pm_ra_cosdec=stars_fromDR3['pmRA'],
+        pm_dec=stars_fromDR3['pmDE'],
+        obstime=(Time('J2000') + 1 * u.Myr)
+    )
+    end_time = time.time()
+    print(f"found {len(stars_fromDR3):,} sources in {end_time - start_time:.2f} seconds")
+    return stars_fromDR3
+
+def searchDR3_dist(skycoordinate: SkyCoord, radius: Angle) -> Table:
+    start_time = time.time()
+    print(f"Searching in Gaia DR3 distances I/352/gedr3dis {radius} around {skycoordinate.ra, skycoordinate.dec, skycoordinate.distance}")
+    stars_fromDR3_dist = Vizier(columns=["*", "+_r"], row_limit=-1).query_region(
+        skycoordinate, 
+        radius=radius, 
+        catalog="I/352/gedr3dis"
+    )[0]
+    stars_fromDR3_dist['SkyCoord2'] = SkyCoord(
+        ra=stars_fromDR3_dist['RA_ICRS'],
+        dec=stars_fromDR3_dist['DE_ICRS'],
+        distance=(stars_fromDR3_dist['rgeo']),
+        obstime=(Time('J2000') + 1 * u.Myr)
+    )
+    end_time = time.time()
+    print(f"found {len(stars_fromDR3_dist):,} sources in {end_time - start_time:.2f} seconds")
+    # filtered_catalog = stars_fromDR3_dist[skycoordinate.separation_3d(stars_fromDR3_dist['SkyCoord2']) < skycoordinate.distance*config['Cluster']['distance_tolerance']]
+    return stars_fromDR3_dist
+
+def merge_gaia_tables(stars_fromDR3: Table, stars_fromDR3_dist: Table) -> Table:
+    start_time = time.time()
+    print("Starting merge of DR3 and distance catalog data")
+
+    warnings.simplefilter('ignore', MergeConflictWarning)
+    merged = join(stars_fromDR3, stars_fromDR3_dist, keys='Source', join_type='inner')
+
+    # Order the columns
+    merged = merged['RA_ICRS_1', 'DE_ICRS_1', 'e_RA_ICRS', 'e_DE_ICRS', '_r_1',
+                                       'HIP', 'TYC2', 'Source', 'rgeo', 'Plx', 'e_Plx',
+                                       'pmRA', 'pmDE', 'e_pmRA', 'e_pmDE',
+                                       'RUWE', 'Teff', 'logg', 'Gmag', 'BP-RP', 'BPmag', 'RPmag', 'RV', 'e_RV',
+                                       'b_rgeo', 'B_rgeo', 'FG', 'e_FG', 'FBP', 'e_FBP', 'FRP', 'e_FRP', 'RAVE5', 'RAVE6']
+
+    # Adding row for uncertainties in Gmag and BPmag and RPmag
+    # values for Gaia G, G_BP, G_RP zero point uncertainties
+    #refer https://astronomy.stackexchange.com/questions/38371/how-can-i-calculate-the-uncertainties-in-magnitude-like-the-cds-does
+    #refer https://www.cosmos.esa.int/web/gaia/edr3-passbands
+    sigmaG_0 = 0.0027553202
+    sigmaGBP_0 = 0.0027901700
+    sigmaGRP_0 = 0.0037793818
+
+    merged['e_Gmag'] = np.sqrt((-2.5 / np.log(10) * merged['e_FG'] / merged['FG'])**2 + sigmaG_0**2)
+    merged['e_BPmag'] = np.sqrt((-2.5 / np.log(10) * merged['e_FBP'] / merged['FBP'])**2 + sigmaGBP_0**2)
+    merged['e_RPmag'] = np.sqrt((-2.5 / np.log(10) * merged['e_FRP'] / merged['FRP'])**2 + sigmaGRP_0**2)
+    merged['e_BP-RP'] = merged['e_BPmag'] + merged['e_RPmag']
+
+    merged['SkyCoord'] = SkyCoord(
+        ra=merged['RA_ICRS_1'],
+        dec=merged['DE_ICRS_1'],
+        distance=(merged['rgeo']),
+        pm_ra_cosdec=merged['pmRA'],
+        pm_dec=merged['pmDE'],
+        obstime=(Time('J2000') + 1 * u.Myr)
+    )
+
+    end_time = time.time()
+    print(f"{len(merged):,} sources found by merging in {end_time - start_time:.2f} seconds")
+    return merged
+
+def get_stars_in_region(skycoordinate: SkyCoord, radius: Angle) -> Table:
+    c = skycoordinate
+    t1 = searchDR3(c,radius)
+    t2 = searchDR3_dist(c,radius)
+    t3 = merge_gaia_tables(t1,t2)
+    t3.sort('Gmag')
+    return t3 
